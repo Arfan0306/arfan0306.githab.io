4d22a75476896116711a056cc09b350a66124097
Arfan0306 <99815861+Arfan0306@users.noreply.github.com>
2022-02-18 19:53:18 +0800

    day one
    latihan coding java

---
+++ Latihan.java
@@ -0,0 +1,11 @@
+package ARIL;
+
+public class Latihan {
+
+  public static void main(String[] args) {
+  String nama = "arfan";
+  System.out.println(nama);
+  int a = 2;
+  System.out.println(a);
+  }
+}
