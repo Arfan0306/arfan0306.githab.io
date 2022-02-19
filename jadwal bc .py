5397aa4d01491c2f84193537f82c022b0b2940d7
Arfan0306 <99815861+Arfan0306@users.noreply.github.com>
2022-02-19 12:42:48 +0800

    Python

---
+++ jadwal bc .py
@@ -0,0 +1,18 @@
+print("\nJADWAL PENGAWALAN\n  BASECAMP 021")
+hari=input("masukan hari :")
+if(hari=="senin"):
+	print("PWK A\nSIPIL B\nINF C")
+elif(hari=="selasa"):
+	print("INF A\nSIPIL F\nINF D")
+elif(hari=="rabu"):
+	print("SIPIL A\nINF F\nPWK C")
+elif(hari=="kamis"):
+	print("INF B\nSIPIL C\nINF E")
+elif(hari=="jumat"):
+	print("PWK B\nSIPIL D")
+elif(hari=="sabtu"):
+	print("INF H\nSIPIL E\nINF G")
+elif(hari=="minggu"):
+	print("SATU TIRAKAN")
+else:
+	print("SALAM TEKNIK")
